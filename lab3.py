a68a7652199cd3ab36f5dd4eb8212d9b84e8c7e9
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-03-02 15:05:28 -0700

    Implemented theta prime

--- lab3.py
+++ lab3.py
@@ -2,6 +2,7 @@
 from controller import Robot, Motor, Camera, RangeFinder, Lidar
 import math
 
+
 MAX_SPEED = 7.0  # [rad/s]
 MAX_SPEED_MS = 0.5725 # [m/s]
 AXLE_LENGTH = 0.4044 # m
@@ -25,7 +26,9 @@ part_names = ("head_2_joint", "head_1_joint", "torso_lift_joint", "arm_1_joint",
 # Note that positions for the joints are in radians.
 target_pos = (0.0, 0.0, 0.09, 0.07, 0.26, -3.16, 1.27, 1.32, 0.0, 1.41, 'inf', 'inf')
 robot_parts = []
-
+#x: 2.344960;
+#y: 1.112664;
+#theta:
 for i in range(N_PARTS):
         robot_parts.append(robot.getDevice(part_names[i]))
         robot_parts[i].setPosition(float(target_pos[i]))
@@ -33,8 +36,8 @@ for i in range(N_PARTS):
 
 ##### Note that the following sensors will not be used for Lab 3.######
 
-# We want to just show that these rich sensors are available with Tiago 
-# some of which will be used later.Feel free to play with them. 
+# We want to just show that these rich sensors are available with Tiago
+# some of which will be used later.Feel free to play with them.
 # The Tiago robot has a couple more sensors than the e-Puck.
 # range = robot.getDevice('range-finder')
 # range.enable(timestep)
@@ -52,37 +55,67 @@ pose_theta = 0
 
 vL = 0
 vR = 0
-        
+
+waypoints = [(1.9, -2.5), (1.37, -2.43), (1.44, -3.0)]
+current_waypoint = (2.0, 0.0)
+
 while robot.step(timestep) != -1:
 
     ## webots offer builtin object detectors which will not be used in Lab 3 ###
-    
+
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
 
     # Webots simulates the equivalent of a Mask R-CNN deep learning-based object
     # recognition framework. We print out the names of the objects Tiago sees. You
-    # can plan with this data structure eventually. 
+    # can plan with this data structure eventually.
     #objects = camera.getRecognitionObjects()
     #for i in objects:
-    #    print(i.get_model()) 
+    #    print(i.get_model())
     ##########################################################################
 
-
-    #STEP 1: Calculate the error
-
-    
+    dist_err = math.sqrt(math.pow(pose_x - current_waypoint[0], 2) + math.pow(pose_y - current_waypoint[1], 2))
+    bearing_err = pose_theta - math.atan2(current_waypoint[1] - pose_y, current_waypoint[0] - pose_x)
 
     #STEP 2: Controller (with gains)
 
+    x_gain = 1.5
+    theta_gain = 3.1 
+    x_prime = dist_err * x_gain
+    theta_prime = (theta_gain * bearing_err)
     
-    
+    if(len(waypoints) != 0 and dist_err < 0.1):
+        current_waypoint = waypoints.pop(0)
+
+    print("Distance Error: %f, Bearing Error: %f x prime: %f" % (dist_err, bearing_err, x_prime))
+    print("Target x: %f Target y: %f" % (current_waypoint[0], current_waypoint[1]))
+
     #STEP 3: Compute wheelspeeds
+
     
-    
+    if(bearing_err > 0.01):
+        vR = theta_prime + theta_gain
+        vL = -theta_prime - theta_gain
+    elif(bearing_err < -0.01):
+        vR = -theta_prime - theta_gain
+        vL = theta_prime + theta_gain
+    else: 
+        vR = x_prime + x_gain
+        vL = x_prime + x_gain
 
     #STEP 4: Normalize wheelspeed
+    
+    if (vR > MAX_SPEED):
+        vR = MAX_SPEED
+    elif(vR < -MAX_SPEED):
+        vR = -MAX_SPEED
+    if (vL > MAX_SPEED):
+        VL = MAX_SPEED
+    elif(vL < -MAX_SPEED):
+        vL = -MAX_SPEED
+        
+    #print("Left Velocity: %f, Right Velocity: %f", (vL, vR))
 
     ################# Do not modify this block of the code ########################
     # Odometry code. Don't change speeds after this
@@ -93,5 +126,7 @@ while robot.step(timestep) != -1:
     ##############################################################################
 
     # Enter here functions to send actuator commands
+    robot_parts[MOTOR_LEFT].setVelocity(vL)
+    robot_parts[MOTOR_RIGHT].setVelocity(vR)
    
     
